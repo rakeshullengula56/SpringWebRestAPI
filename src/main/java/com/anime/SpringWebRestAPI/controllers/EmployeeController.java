d4ef11a95a1d462fc5d6d3088bf7771917becb10
rakeshullengula56 <ramullengularakesh@gmail.com>
2024-12-30 22:49:15 +0530

    Learned PathVariables and Query Variables

--- src/main/java/com/anime/SpringWebRestAPI/controllers/EmployeeController.java
+++ src/main/java/com/anime/SpringWebRestAPI/controllers/EmployeeController.java
@@ -1,16 +1,19 @@
 package com.anime.SpringWebRestAPI.controllers;
 
 import com.anime.SpringWebRestAPI.dto.EmployeeDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 @RestController
+@RequestMapping(path="/employees")
 public class EmployeeController {
-    @GetMapping("/employees/{empId}")
-    public EmployeeDTO getEmployeeById(@PathVariable Long empId){
-        return new EmployeeDTO(empId,"Rakesh","ram@gmail.com",22, LocalDate.of(2024,12,30),true);
+    @GetMapping("/{empId}")
+    public EmployeeDTO getEmployeeById(@PathVariable(name="empId") Long Id){
+        return new EmployeeDTO(Id,"Rakesh","ram@gmail.com",22, LocalDate.of(2024,12,30),true);
+    }
+    @GetMapping
+    public String getAllEmployees(@RequestParam(required = false) String age,@RequestParam(required = false) String sortBy){
+        return "Hi all "+age+ " "+sortBy;
     }
 }

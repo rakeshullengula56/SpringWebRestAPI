25faac6e79cd96af367455b6c3b3ff6806c92761
rakeshullengula56 <ramullengularakesh@gmail.com>
2024-12-30 21:11:14 +0530

    learning Spring Web and MVC

---
+++ src/main/java/com/anime/SpringWebRestAPI/SpringWebRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.anime.SpringWebRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anime/SpringWebRestAPI/SpringWebRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anime.SpringWebRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

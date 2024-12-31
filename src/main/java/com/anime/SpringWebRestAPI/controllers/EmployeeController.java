b0879441c3866986492edc6a3fc8f52c25e57357
rakeshullengula56 <ramullengularakesh@gmail.com>
2024-12-31 20:21:01 +0530

    Updated controller and added Lombok dependency

--- src/main/java/com/anime/SpringWebRestAPI/controllers/EmployeeController.java
+++ src/main/java/com/anime/SpringWebRestAPI/controllers/EmployeeController.java
@@ -1,24 +1,37 @@
 package com.anime.SpringWebRestAPI.controllers;
 
 import com.anime.SpringWebRestAPI.dto.EmployeeDTO;
+import com.anime.SpringWebRestAPI.entities.EmployeeEntity;
+import com.anime.SpringWebRestAPI.repositories.EmployeeRepository;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
+import java.util.List;
+
 
 @RestController
-@RequestMapping(path="/employees")
+@RequestMapping(path = "/employees")
 public class EmployeeController {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
     @GetMapping("/{empId}")
-    public EmployeeDTO getEmployeeById(@PathVariable(name="empId") Long Id){
-        return new EmployeeDTO(Id,"Rakesh","ram@gmail.com",22, LocalDate.of(2024,12,30),true);
+    public EmployeeEntity getEmployeeById(@PathVariable(name = "empId") Long id) {
+        return employeeRepository.findById(id).orElse(null); // Fixed syntax for `findById`
     }
+
     @GetMapping
-    public String getAllEmployees(@RequestParam(required = false) String age,@RequestParam(required = false) String sortBy){
-        return "Hi all "+age+ " "+sortBy;
+    public List<EmployeeEntity> getAllEmployees(
+            @RequestParam(required = false) String age,
+            @RequestParam(required = false) String sortBy) {
+        return employeeRepository.findAll();
     }
+
     @PostMapping
-    public EmployeeDTO addEmployee(@RequestBody EmployeeDTO inputEmp){
-        inputEmp.setId(13L);
-        return inputEmp;
+    public EmployeeEntity addEmployee(@RequestBody EmployeeEntity inputEmp) {
+        return employeeRepository.save(inputEmp);
     }
 }
+

a41bce13b3c9d2b1a9ee9cf15cf177759279d938
rakeshullengula56 <ramullengularakesh@gmail.com>
2024-12-31 20:13:51 +0530

    created entity class

---
+++ src/main/java/com/anime/SpringWebRestAPI/entities/EmployeeEntity.java
@@ -0,0 +1,19 @@
+package com.anime.SpringWebRestAPI.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="employees")
+public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String email;
+    private Integer age;
+    private LocalDate dateOfJoining;
+    private Boolean isActive;
+}
